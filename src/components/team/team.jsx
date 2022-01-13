60984914e44d9c46d745481d7d6fc0483470c77a
changlmasaki <59718659+changlmasaki@users.noreply.github.com>
2022-01-12 20:49:38 -0500

    JIRA1

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -6,29 +6,30 @@ import TeamBox from './teamBox';
 import TeamInfo from "./teamInfo";
 import Title from "../ui-components/title/title";
 // Assets
-import Person01 from "../../assets/about/bing.jpg"
-import Person02 from "../../assets/about/steward.jpg"
-import Person03 from '../../assets/about/sparaggon.jpg'
-import Person04 from '../../assets/about/mahin.jpeg'
-import Person05 from '../../assets/about/robin.jpeg'
+import Person01 from "../../assets/about/ceo.jpg"
+import Person02 from "../../assets/about/p1.jpg"
+import Person03 from '../../assets/about/p2.jpg'
+import Person04 from '../../assets/about/p3.png'
+import Person05 from '../../assets/about/p4.png'
 
 const about = () => (
   <div id="about">
     <div className="wrapper">
-      <Title title="The TEAM" />
+      <Title title="Professional Team" />
       <p className="font12">
       </p>
       <Row>
         <Col md={12} lg={2}>
         </Col>
         <Col md={12} lg={3}>
-          <TeamBox avatar={Person01} name="Bing" job="Founder" />
+          <TeamBox avatar={Person01} name="George" job="CEO" />
         </Col>
         <Col md={12} lg={5}>
           <div className="team__info flex-center">
             <div>
               <p className="font12 weight500">
-                Bing has extensive experience in running startups from different industries (including cannabis). He had successfully initiated and exited few startups in the past decade. Furthermore, he has tremendous resources and experiences in the venture capital and private equity market as he has served as advisor and partner for different financial firms. Bing graduated from University of Toronto with an Honor B.A degree majoring in Economics and Math.
+              George A.Brown is the co-founder and CEO of Cannvalue. Prior to Cannvalue, George was CEO of Biotech Nas, a biotech research organzation in BC. George
+              holds an MBA from Stanford Graduate School of Business. 
              </p>
             </div>
           </div>
@@ -38,25 +39,25 @@ const about = () => (
       </Row>
       <Row>
         <Col md={12} lg={3}>
-          <TeamBox avatar={Person02} name="Steward" job="Sales director" />
+          <TeamBox avatar={Person02} name="Steven" job="Sales director" />
         </Col>
         <Col md={12} lg={3}>
           <div className="team__info flex-center">
             <div>
               <p className="font12 weight500">
-              Steward has been in the sales profession for over 20 years, mainly in cannabis, nicotine, and tobacco industries. Companies he has served ranging from startups to internationally well known corporations which has given him tremendous resources and connections in the industry.
+              Steven has been in the sales profession for over 20 years, mainly in cannabis, nicotine, and tobacco industries. Companies he has served ranging from startups to internationally well known corporations which has given him tremendous resources and connections in the industry.
              </p>
             </div>
           </div> 
         </Col>
         <Col md={12} lg={3}>
-          <TeamBox avatar={Person03} name="Sparaggon" job="VP of Sales & Marketing" />
+          <TeamBox avatar={Person03} name="Sam" job="VP of Sales & Marketing" />
         </Col>
         <Col md={12} lg={3}>
           <div className="team__info flex-center">
             <div>
               <p className="font12 weight500">
-              Sparaggon graduated from university of waterloo with a degree in Bachelor of Science, Sparragon has over 20 years of sales & marketing professional experience for both the pharmaceutical and cannabis industry. In Canadian legalized cannabis market, Sparragon has demonstrated his capabilities of deep connection with both top tier LPs and small sized brands.
+              Sam graduated from university of Toronto with a degree in Bachelor of Science, Sam has over 20 years of sales & marketing professional experience for both the pharmaceutical and cannabis industry. In Canadian legalized cannabis market, Sam has demonstrated his capabilities of deep connection with both top tier LPs and small sized brands.
              </p>
             </div>
           </div>
@@ -64,25 +65,25 @@ const about = () => (
       </Row>
       <Row>
         <Col md={12} lg={3}>
-          <TeamBox avatar={Person04} name="Mahin" job="R&D Director" />
+          <TeamBox avatar={Person04} name="Matten" job="legal advisor" />
         </Col>
         <Col md={12} lg={3}>
           <div className="team__info flex-center">
             <div>
               <p className="font12 weight500">
-              Mahin holds two master of science degrees with a major in mechanical engineering from both UK and Canadian’ top universities. He has been working in R&D field for both startups and top e-cig brands for nearly a decade.
+              Mahin holds two master degrees with in Law Schools from both UK and Canadian’ top universities. He has been working in legal consultation field for nearly a decade.
              </p>
             </div>
           </div>
         </Col>
         <Col md={12} lg={3}>
-          <TeamBox avatar={Person05} name="Robin" job="IT director" />
+          <TeamBox avatar={Person05} name="David" job="IT director" />
         </Col>
         <Col md={12} lg={3}>
           <div className="team__info flex-center">
             <div>
               <p className="font12 weight500">
-              Graduated from University of Toronto with specialist in computer science and software engineering, Robin has been working as a Lead full stack software developer for multiple top Canadian financial institutions during the past 8+ years. 
+              Graduated from University of British Columbia with specialist in computer science and software engineering, David has been working as a senior software developer for top Canadian IT companies during the past 5 years. 
              </p>
             </div>
           </div>
